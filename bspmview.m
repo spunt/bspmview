54d55eaf3f3f14e8151b0a9fdeba5303a5dac066
Bob Spunt <bobspunt@gmail.com>
2015-02-20 10:58:50 -0800

    added license info

--- bspmview.m
+++ bspmview.m
@@ -33,11 +33,13 @@ function S = bspmview(ol, ul)
 % 	(http://www.alivelearn.net/xjview8/developers/), and FIVE.m by Aaron P.
 % 	Schultz (http://mrtools.mgh.harvard.edu/index.php/Main_Page). This also
 % 	employs some of the functionality of PEAK_NII.m by Donald McLaren
-% 	(http://www.nmr.mgh.harvard.edu/~mclaren/ftp/Utilities_DGM/). Finally,
+% 	(http://www.nmr.mgh.harvard.edu/~mclaren/ftp/Utilities_DGM/). Moreover,
 % 	several contributions to the MATLAB File Exchange
 % 	(http://www.mathworks.com/matlabcentral/fileexchange/) are called by
 % 	the code. These are included in the "supporting files" folder that should 
 %   have been included in the distribution of the main BSPMVIEW function.
+%   The documentation of the supporting functions contains further
+%   information about the source and respective copyright holders. 
 % 
 
 % ------ Copyright (C) Bob Spunt, California Institute of Technology ------
@@ -45,6 +47,17 @@ function S = bspmview(ol, ul)
 %	Created:  2014-09-27
 %   GitHub:   https://github.com/spunt/bspmview
 %   Version:  20150220
+%
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or (at
+%   your option) any later version.
+%       This program is distributed in the hope that it will be useful, but
+%   WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%   General Public License for more details.
+%       You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see: http://www.gnu.org/licenses/.
 % _________________________________________________________________________
 version = '20150220'; 
 
